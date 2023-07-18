a8a701fb62d4cb513a06390d36ea9f2fec83f768
Even Rouault <even.rouault@spatialys.com>
2023-07-18 11:33:59 +0200

    BAG: revert removal of interpolated resampled raster done in 34ea11fe03c5b85b927cc11c76a13c39026e7d82 per #2865

--- autotest/gdrivers/bag.py
+++ autotest/gdrivers/bag.py
@@ -711,6 +711,39 @@ def test_bag_vr_resampled_mask():
 #
 
 
+def test_bag_vr_resampled_interpolated():
+    if gdaltest.bag_drv is None:
+        pytest.skip()
+
+    ds = gdal.OpenEx(
+        "data/bag/test_vr.bag",
+        open_options=["MODE=RESAMPLED_GRID", "INTERPOLATION=INVDIST"],
+    )
+    cs = ds.GetRasterBand(1).Checksum()
+    assert cs == 2107
+
+    # Test overviews
+    with gdaltest.config_option("GDAL_BAG_MIN_OVR_SIZE", "4"):
+        ds = gdal.OpenEx("data/bag/test_vr.bag", open_options=["MODE=RESAMPLED_GRID"])
+    assert ds.GetRasterBand(1).GetOverviewCount() == 2
+
+    # Incompatible options
+    with gdaltest.error_handler():
+        ds = gdal.OpenEx(
+            "data/bag/test_vr.bag",
+            open_options=[
+                "MODE=RESAMPLED_GRID",
+                "SUPERGRIDS_MASK=YES",
+                "INTERPOLATION=INVDIST",
+            ],
+        )
+    assert ds is None
+
+
+###############################################################################
+#
+
+
 def test_bag_write_single_band():
 
     tst = gdaltest.GDALTest("BAG", "byte.tif", 1, 4672)
