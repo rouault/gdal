408a1dde73a78627cf0124c62e9705617ba87874
Even Rouault <even.rouault@spatialys.com>
2024-04-04 02:16:20 +0200

    autotest: Avoid random failure on test_gdal_proximity.py and test_gdal_sieve.py with ASAN

--- autotest/pyscripts/test_gdal_proximity.py
+++ autotest/pyscripts/test_gdal_proximity.py
@@ -31,6 +31,7 @@
 
 import os
 
+import gdaltest
 import pytest
 import test_py_scripts
 
@@ -52,6 +53,9 @@ def script_path():
 
 
 def test_gdal_proximity_help(script_path):
+
+    if gdaltest.is_travis_branch("sanitize"):
+        pytest.skip("fails on sanitize for unknown reason")
 
     assert "ERROR" not in test_py_scripts.run_py_script(
         script_path, "gdal_proximity", "--help"
