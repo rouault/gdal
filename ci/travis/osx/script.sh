a4b3f64c96e8046e6eea96a9747c56e7bae7a1f7
Even Rouault <even.rouault@spatialys.com>
2024-04-30 19:08:52 +0200

    CI: debug mac issue

--- ci/travis/osx/script.sh
+++ ci/travis/osx/script.sh
@@ -14,6 +14,17 @@ sudo -H pip3 install -r autotest/requirements.txt
 NPROC=$(sysctl -n hw.ncpu)
 echo "NPROC=${NPROC}"
 
+export DYLD_LIBRARY_PATH=$PWD/build
+
+echo "otool -L build/libgdal.dylib"
+otool -L build/libgdal.dylib
+
+echo "otool -L build/swig/python/osgeo/_gdal.cpython*"
+otool -L build/swig/python/osgeo/_gdal.cpython*
+
+export PYTHONPATH=$PWD/build/swig/python
+python3 -c "from osgeo import gdal"
+
 # Run all the Python autotests
 # FIXME: disabled for now because of https://github.com/OSGeo/gdal/issues/9723
 #(cd build && ctest -V -R autotest -j${NPROC})
