94dc28c7fe51dd931fc6ad20ff70842847257879
Even Rouault <even.rouault@spatialys.com>
2024-06-13 16:06:54 +0200

    OGRSpatialReference::importFromUSGS(): use plain WGS84 datum with WGS84 ellipsoid, and identify EPSG code

--- ogr/ogr_srs_usgs.cpp
+++ ogr/ogr_srs_usgs.cpp
@@ -734,8 +734,14 @@ OGRErr OGRSpatialReference::importFromUSGS(long iProjSys, long iZone,
         }
         else if (iDatum < NUMBER_OF_ELLIPSOIDS && aoEllips[iDatum])
         {
-            if (OSRGetEllipsoidInfo(aoEllips[iDatum], &pszName, &dfSemiMajor,
-                                    &dfInvFlattening) == OGRERR_NONE)
+            if (aoEllips[iDatum] == 7030)  // WGS 84 ellipsoid
+            {
+                // Assume a WGS 84 datum
+                SetWellKnownGeogCS("WGS84");
+            }
+            else if (OSRGetEllipsoidInfo(aoEllips[iDatum], &pszName,
+                                         &dfSemiMajor,
+                                         &dfInvFlattening) == OGRERR_NONE)
             {
                 SetGeogCS(
                     CPLString().Printf(
@@ -772,6 +778,17 @@ OGRErr OGRSpatialReference::importFromUSGS(long iProjSys, long iZone,
     /* -------------------------------------------------------------------- */
     if (IsLocal() || IsProjected())
         SetLinearUnits(SRS_UL_METER, 1.0);
+
+    if (iDatum < NUMBER_OF_ELLIPSOIDS && aoEllips[iDatum] == 7030)
+    {
+        if (AutoIdentifyEPSG() == OGRERR_NONE)
+        {
+            const char *pszAuthName = GetAuthorityName(nullptr);
+            const char *pszAuthCode = GetAuthorityCode(nullptr);
+            if (pszAuthName && pszAuthCode && EQUAL(pszAuthName, "EPSG"))
+                CPL_IGNORE_RET_VAL(importFromEPSG(atoi(pszAuthCode)));
+        }
+    }
 
     return OGRERR_NONE;
 }
